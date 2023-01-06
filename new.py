45d263f5b324d2189cbfea4725a36ab83e0c9e8e
tushar9596 <tsaste0011@gmail.com>
2023-01-06 17:00:58 +0530

    iam in developer branch,trying to push this branch

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
 print("hey there, ")
 print("iam in developer2 branch")
 print("iam learning how to create new branches and merge with the main branch")
+print("iam in developer branch")
+print("iam trying to push the branch")
